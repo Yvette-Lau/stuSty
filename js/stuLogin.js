8810f42088f69e24d960690a802449bd34e38d9d
Yvette <lauyatting9598@gmail.com>
2017-04-21 12:57:18 +0800

    jQuery validator

--- js/stuLogin.js
+++ js/stuLogin.js
@@ -15,3 +15,56 @@ $(function() {
 		e.preventDefault();
 	});
 });
+
+
+
+
+$(document).ready(function(){
+
+	$("#register-form").validate({
+		errorContainer: "div.error",
+		errorLabelContainer: $("#signupForm div.error"),
+		wrapper: "li",
+		rules:{
+			username:{
+				required:true,
+				minlength:6,
+				maxlength:20
+			},
+			password:{
+				required:true,
+				minlength:6,
+				maxlength:25
+			},
+			confirm_password:{
+				required:true,
+				equalTo:"#password"
+			},
+			email:{
+				required:true,
+				email:true
+			},
+		},
+		messages:{
+			username:{
+				required:"请输入用户名",
+				minlength:"用户名必须6到20字符之间",
+				maxlength:"用户名必须6到20字符之间"
+			},
+			password:{
+				required:"请输入密码",
+				minlength:"密码必须6到25字符之间",
+				maxlength:"密码必须6到25字符之间"
+			},
+			confirm_password:{
+				required:"请输入密码",
+				equalTo:"两次密码输入不一致"
+			},
+			email:"请输入一个正确的邮箱",
+		},
+		submitHandler:function(form) {
+	      alert("注册成功！");
+	      form.submit();
+	    }
+	});
+});
